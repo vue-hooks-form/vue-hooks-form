dea020b25d4be11896d5f200927c3a85c19be062
elonehoo <elonehoo@gmail.com>
2023-01-07 20:45:02 +0800

    fix: modify the package type

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,29 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { createErrorHandler, createSubmitHandler, useForm } from '@vue-hooks-form/core'
+
+const {
+  register,
+  formState: { errors },
+  isExistInErrors,
+  handleSubmit,
+} = useForm({
+  mode: 'onChange'
+})
+
+const onSubmit = createSubmitHandler((data:any) => {
+  alert('submit success')
+})
+
+const onError = createErrorHandler((errors:any) => {
+  alert('submit failed')
+})
 </script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  {{ errors }}
+  <form @submit.prevent="handleSubmit(onSubmit, onError)()">
+    name: <input :="register('name', { required: true })"/>
+    <button type="submit">submit</button>
+  </form>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
