4c03c099261ae04a846dd101ae394eb5e5189b82
elonehoo <hi@elonehoo.me>
2023-11-16 10:00:08 +0800

    chore: import

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,12 @@
 export * from './useForm'
 export * from './useFieldArray'
 export * from './utils/index'
+import set from 'lodash.setwith'
+import get from 'lodash.get'
+import unset from 'lodash.unset'
+
+export {
+  set,
+  get,
+  unset
+}
