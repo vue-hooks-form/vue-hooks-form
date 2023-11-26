8c2062523d1384b3b8e04c7db89c3dca98d4232e
Kyle <lauber.kyle@gmail.com>
2023-11-25 18:41:04 -0800

    chore: export types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export * from './useForm'
 export * from './useFieldArray'
 export * from './utils/index'
+export * from './types'
 import set from 'lodash.setwith'
 import get from 'lodash.get'
 import unset from 'lodash.unset'
